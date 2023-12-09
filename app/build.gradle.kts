88d2081ae15d840e8ff63844caba1d3102ce2892
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-12-08 22:13:35 -0300

    adiciona as dependencias do room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 }
 
 dependencies {
+    // adiciona as dependências do room do sqlite
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
