96464b32340b6a367c28789e41b8dac1f564401d
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-12-09 02:18:36 -0300

    atualiza configuracao room

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,21 +40,28 @@ android {
 }
 
 dependencies {
-    // adiciona as dependências do room do sqlite
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // adiciona as dependências do Room, SQLite e outras dependências do Android
 
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
+    // dependências principais do Android
+    implementation("androidx.core:core-ktx:1.12.0") // Funcionalidades principais do Android com suporte para Kotlin
+    implementation("androidx.appcompat:appcompat:1.6.1") // Biblioteca de compatibilidade com versões antigas do Android
+    implementation("com.google.android.material:material:1.10.0") // Biblioteca de design de Material Components
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4") // Layouts avançados para Android
 
-    implementation ("androidx.room:room-runtime:2.6.0")
-    implementation ("androidx.room:room-ktx:2.6.0")
-    ksp("androidx.room:room-compiler:2.6.0")
+    // Dependências de navegação
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5") // Componentes de fragmento para navegação
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5") // Componentes de UI para navegação
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Dependências do Room (Biblioteca de persistência)
+    implementation ("androidx.room:room-runtime:2.6.0") // Biblioteca principal do Room
+    implementation ("androidx.room:room-ktx:2.6.0") // Extensões Kotlin para Room
+    ksp("androidx.room:room-compiler:2.6.0") // Processador de anotações para geração de código no Room
+
+    // ksp -> Kotlin Symbol Processing
+
+    // Dependências para testes
+    testImplementation("junit:junit:4.13.2") // Framework de testes para Java
+    androidTestImplementation("androidx.test.ext:junit:1.1.5") // Extensões de teste para Android (Junit)
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1") // Framework para testes de UI automatizados no Android (Espresso)
 }
