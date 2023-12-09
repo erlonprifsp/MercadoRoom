7438bcf5e713128c6b9a1d29363a76112d7e5196
erlonprifsp <erlon.rodrigues@aluno.ifsp.edu.br>
2023-12-08 22:31:09 -0300

    cria o banco de dados

--- app/src/main/java/br/ifsp/mercadoroom/data/ProdutoDatabase.kt
+++ app/src/main/java/br/ifsp/mercadoroom/data/ProdutoDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+// define o banco de dados que utiliza as entidades e os DAOs
 
 @Database(entities = [Produto::class], version = 1)
 abstract class ProdutoDatabase: RoomDatabase() {
